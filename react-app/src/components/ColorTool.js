6ae9c98f43c5fd1ba08899ea2000438d7797ba11
Eric Greene <eric@t4d.io>
2022-05-16 18:54:51 -0400

    add exercise 5

--- react-app/src/components/ColorTool.js
+++ react-app/src/components/ColorTool.js
@@ -1,4 +1,6 @@
-export const ColorTool = (props) => {
+import PropTypes from 'prop-types';
+
+export const ColorTool = ({ colors }) => {
 
   return (
     <>
@@ -6,9 +8,20 @@ export const ColorTool = (props) => {
         <h1>Color Tool</h1>
       </header>
       <ul>
-        {props.colors.map(color => <li key={color.id}>{color.name}</li>)}
+        {colors.map(({ id, name }) => <li key={id}>{name}</li>)}
       </ul>
     </>
   );
+};
 
+ColorTool.defaultProps = {
+  colors: [],
+};
+
+ColorTool.propTypes = {
+  colors: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    hexcode: PropTypes.string.isRequired,
+  })).isRequired,
 };
