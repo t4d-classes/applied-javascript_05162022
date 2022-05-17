f853b7d9df33cca60d635472a2caf4d2eec36236
Eric Greene <eric@t4d.io>
2022-05-17 16:53:10 -0400

    add exercise 8

--- react-app/src/components/CarForm.js
+++ react-app/src/components/CarForm.js
@@ -1,21 +1,57 @@
-export const CarForm = ({ buttonText }) => {
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: block;
+`;
+
+export const CarForm = ({ buttonText, onSubmitCar }) => {
+
+  const [ carForm, setCarForm ] = useState({
+    make: '', model: '', year: 1900, color: '', price: 0,
+  });
+
+  const change = (evt) => {
+    setCarForm({
+      ...carForm,
+      [evt.target.name]: evt.target.type === 'number'
+        ? evt.target.valueAsNumber
+        : evt.target.value,
+    });
+  };
+
+  const submitCar = () => {
+    onSubmitCar({...carForm });
+  };
 
   return (
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Hexcode:
-        <input type="text" name="hexcode" />
-      </label>
-      <button type="button">{buttonText}</button>
+    <form className="carForm">
+      <Label>
+        Make:
+        <input type="text" name="make" value={carForm.make} onChange={change} />
+      </Label>
+      <Label>
+        Model:
+        <input type="text" name="model" value={carForm.model} onChange={change} />
+      </Label>
+      <Label>
+        Year:
+        <input type="number" name="year" value={carForm.year} onChange={change} />
+      </Label>
+      <Label>
+        Color:
+        <input type="text" name="color" value={carForm.color} onChange={change} />
+      </Label>
+      <Label>
+        Price:
+        <input type="number" name="price" value={carForm.price} onChange={change} />
+      </Label>
+      <button type="button" onClick={submitCar}>{buttonText}</button>
     </form>
   );
 
 };
 
 CarForm.defaultProps = {
-  buttonText: "Submit Color",
+  buttonText: "Submit Car",
 };
