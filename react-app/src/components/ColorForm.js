b2718858811298b9f777cd2035e8f3f4ec157e04
Eric Greene <eric@t4d.io>
2022-05-17 14:45:21 -0400

    add exercise 7

--- react-app/src/components/ColorForm.js
+++ react-app/src/components/ColorForm.js
@@ -1,60 +1,26 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-// import './ColorForm.css';
-
-// every time the state changes, the functions runs again to re-render the component
-
 const Label = styled.label`
   display: block;
 `;
 
-export const ColorForm = ({ buttonText }) => {
+export const ColorForm = ({ buttonText, onSubmitColor }) => {
 
-  // const colorFormState = useState({
-  //   name: '',
-  //   hexcode: '',
-  // } /* initializes the state on the first render */);
-
-  // const colorForm = colorFormState[0];
-
-  // const setColorForm = colorFormState[1];
-
-  // using the array as a tuple structure
   const [ colorForm, setColorForm ] = useState({
     name: '', hexcode: '',
   });
 
-  // setColorForm({
-  //  name:'purple', hexcode: 'ff00ff'
-  // })
-
   const change = (evt) => {
-
-    // // extract the name and value from the input field
-    // const controlValue = evt.target.value;
-    // const controlName = evt.target.name; // name or hexcode
-
-    // // making a copy of the color form object
-    // const newColorForm = {
-    //   name: colorForm.name,
-    //   hexcode: colorForm.hexcode,
-    // };
-
-    // // updates the one field for the one control we were typing into
-    // newColorForm[controlName] = controlValue;
-
-    // // updates the state and triggers a re-render, which mean the component
-    // // function will execute again
-    // setColorForm(newColorForm);
-
     setColorForm({
       ...colorForm,
       [evt.target.name]: evt.target.value,
     });
   };
 
-  console.log(colorForm);
+  const submitColor = () => {
+    onSubmitColor({...colorForm });
+  };
 
   return (
     <form className="colorForm">
@@ -66,7 +32,7 @@ export const ColorForm = ({ buttonText }) => {
         Hexcode:
         <input type="text" name="hexcode" value={colorForm.hexcode} onChange={change} />
       </Label>
-      <button type="button">{buttonText}</button>
+      <button type="button" onClick={submitColor}>{buttonText}</button>
     </form>
   );
 
--- react-app/src/components/ColorTool.js
+++ react-app/src/components/ColorTool.js
@@ -1,16 +1,31 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 import { ToolHeader } from './ToolHeader';
 import { ColorList } from './ColorList';
 import { ColorForm } from './ColorForm';
 
-export const ColorTool = ({ colors }) => {
+export const ColorTool = ({ colors: initialColors }) => {
+
+  const [ colors, setColors ] = useState([ ...initialColors ]);
+
+  const addColor = newColor => {
+
+    setColors([
+      ...colors,
+      {
+        ...newColor,
+        id: Math.max(...colors.map(c => c.id), 0) + 1,
+      },
+    ]);
+
+  };
 
   return (
     <>
       <ToolHeader headerText="Color Tool" />
       <ColorList colors={colors} />
-      <ColorForm buttonText="Add Color" />
+      <ColorForm buttonText="Add Color" onSubmitColor={addColor} />
     </>
   );
 };
