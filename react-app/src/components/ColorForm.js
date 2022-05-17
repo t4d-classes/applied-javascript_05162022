7c13b3b38e28b9997225942786d699768e56ed67
Eric Greene <eric@t4d.io>
2022-05-17 13:50:22 -0400

    add shorter code for color form

--- react-app/src/components/ColorForm.js
+++ react-app/src/components/ColorForm.js
@@ -11,14 +11,19 @@ const Label = styled.label`
 
 export const ColorForm = ({ buttonText }) => {
 
-  const colorFormState = useState({
-    name: '',
-    hexcode: '',
-  } /* initializes the state on the first render */);
+  // const colorFormState = useState({
+  //   name: '',
+  //   hexcode: '',
+  // } /* initializes the state on the first render */);
 
-  const colorForm = colorFormState[0];
+  // const colorForm = colorFormState[0];
 
-  const setColorForm = colorFormState[1];
+  // const setColorForm = colorFormState[1];
+
+  // using the array as a tuple structure
+  const [ colorForm, setColorForm ] = useState({
+    name: '', hexcode: '',
+  });
 
   // setColorForm({
   //  name:'purple', hexcode: 'ff00ff'
@@ -26,22 +31,27 @@ export const ColorForm = ({ buttonText }) => {
 
   const change = (evt) => {
 
-    // extract the name and value from the input field
-    const controlValue = evt.target.value;
-    const controlName = evt.target.name; // name or hexcode
+    // // extract the name and value from the input field
+    // const controlValue = evt.target.value;
+    // const controlName = evt.target.name; // name or hexcode
 
-    // making a copy of the color form object
-    const newColorForm = {
-      name: colorForm.name,
-      hexcode: colorForm.hexcode,
-    };
+    // // making a copy of the color form object
+    // const newColorForm = {
+    //   name: colorForm.name,
+    //   hexcode: colorForm.hexcode,
+    // };
 
-    // updates the one field for the one control we were typing into
-    newColorForm[controlName] = controlValue;
+    // // updates the one field for the one control we were typing into
+    // newColorForm[controlName] = controlValue;
 
-    // updates the state and triggers a re-render, which mean the component
-    // function will execute again
-    setColorForm(newColorForm);
+    // // updates the state and triggers a re-render, which mean the component
+    // // function will execute again
+    // setColorForm(newColorForm);
+
+    setColorForm({
+      ...colorForm,
+      [evt.target.name]: evt.target.value,
+    });
   };
 
   console.log(colorForm);
