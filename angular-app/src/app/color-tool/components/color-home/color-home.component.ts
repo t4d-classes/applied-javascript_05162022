b98ee3902c2060165dff9981a8736bc8bb0c81dd
Eric Greene <eric@t4d.io>
2022-05-18 16:53:27 -0400

    add exercise 2

--- angular-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ angular-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Color } from '../../models/colors';
+
 @Component({
   selector: 'app-color-home',
   templateUrl: './color-home.component.html',
   styleUrls: ['./color-home.component.css']
 })
 export class ColorHomeComponent implements OnInit {
+
+  headerText = "Color Tool";
+
+  colors: Color[] = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+  ];
 
   constructor() { }
 
