634abb451ea110f248df9368aa616f7feb9a1387
Eric Greene <eric@t4d.io>
2022-05-20 17:56:14 -0400

    add put endpoint

--- full-stack-app/express-server-app/routes.js
+++ full-stack-app/express-server-app/routes.js
@@ -20,5 +20,74 @@ export const createAPI = (resourceName, Model) => {
       });
 
     })
+    .post((req, res) => {
+
+      const newResource = new Model({
+        ...req.body,
+      });
+
+      newResource.save((err, resource) => {
+
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+
+        res.json(resource.toObject());
+
+      });
+
+    });
+
+  router.route(`/${resourceName}/:resourceId`)
+    .get((req, res) => {
+
+      Model.findOne({ _id: req.params.resourceId }, (err, resource) => {
+
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+
+        res.json(resource.toObject());
+
+      });      
+
+    })
+    .put((req, res) => {
+
+      Model.findOne({ _id: req.params.resourceId }, (err, resource) => {
+
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+
+        const oldResource = {
+          ...resource,
+        };
+
+        const updatedCar = { ...req.body };
+        delete updatedCar.id;
+
+        Model.updateOne(
+          { _id: req.params.resourceId },
+          Object.assign(resource, updatedCar),
+          err => {
+
+            if (err) {
+              console.log(err);
+              res.sendStatus(500);
+              return;
+            }
+            
+            res.json(oldResource.toObject());
+          }
+        );
+      });      
+    })
 
 };
